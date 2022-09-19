226c9d94c35832e95b231ea948cbcf68d11308ed
anavrin08 <aalisonpedrozo@gmail.com>
2022-09-19 22:41:57 +0800

    commit2

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.testFunction = void 0;
+exports.displayDirectory = exports.addCity = exports.testFunction = void 0;
 const testFunction = () => {
-    console.log("Hello to Typescript");
+    console.log("Hello Cold World");
 };
 exports.testFunction = testFunction;
+//displaying in html
+var cityDirectory = [];
+const addCity = (city) => {
+    cityDirectory.push(city);
+    (0, exports.displayDirectory)();
+};
+exports.addCity = addCity;
+const displayDirectory = () => {
+    let list = document.getElementById('cityDirectory');
+    if (list) {
+        while (list.hasChildNodes() && list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+    }
+    cityDirectory.forEach((item) => {
+        if (list) {
+            let li = document.createElement("li");
+            li.innerText = item;
+            list.appendChild(li);
+        }
+    });
+};
+exports.displayDirectory = displayDirectory;
 (0, exports.testFunction)();
--- index.ts
+++ index.ts
@@ -1,5 +1,30 @@
 export const testFunction = () => {
-    console.log("Hello to Typescript")
+    console.log("Hello Cold World")
 }
+
+//displaying in html
+var cityDirectory:string[] = [];
+
+export const addCity = (city:string) => {
+    cityDirectory.push(city);
+    displayDirectory();
+}
+
+export const displayDirectory = () => {
+    let list = document.getElementById('cityDirectory');
+    if(list) {
+        while(list.hasChildNodes() && list.firstChild) {
+            list.removeChild(list.firstChild)
+        }
+    }
+    cityDirectory.forEach((item) => {
+        if(list) {
+            let li = document.createElement("li");
+            li.innerText = item
+            list.appendChild(li)
+        }
+    })
+}
+
 
 testFunction();
